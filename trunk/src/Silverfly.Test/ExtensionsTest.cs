a0799a02de1dda27cf87c21caa4790ea460d912a
nick.hadfield <nick.hadfield@bb6472a4-7c49-11de-a62b-11094ef63f4d>
2009-08-06 06:03:42 +0000

    Added Marshall() extension method for generic event handlers.

---
+++ trunk/src/Silverfly.Test/ExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace Silverfly.Test
+{
+    /// <summary>
+    /// Summary description for ExtensionsTest
+    /// </summary>
+    [TestClass]
+    public class ExtensionsTest
+    {
+        bool isCompleted = true;
+
+        [TestMethod]
+        public void CanMarshallGenericEventHandler()
+        {
+            EventHandler<EventArgs> handler = ((EventHandler<EventArgs>)OnCompletion).Marshall();
+            var thread = new Thread(
+                new ThreadStart(() =>
+                {
+                    handler(this, new EventArgs());
+                }
+                )
+            );
+            thread.Start();
+            thread.Join();
+            Assert.IsTrue(isCompleted, "Event was not handled.");
+        }
+
+        public void OnCompletion(object sender, EventArgs ea)
+        {
+            isCompleted = true;
+            Console.WriteLine("Completed");
+        }
+    }
+}
---
+++ trunk/src/Silverfly/Extensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Silverfly
+{
+    /// <summary>
+    /// Extension methods for silverfly.
+    /// </summary>
+    public static class Extensions
+    {
+        #region Threading
+
+        /// <summary>
+        /// Marshalls an event handler to the current synchronization context.
+        /// </summary>
+        /// <param name="handler">The handler to be marshalled.</param>
+        /// <returns>A new event handler proxy the marshalls the handler to the current synchronization context.</returns>
+        public static EventHandler<TEventArgs> Marshall<TEventArgs>(
+            this EventHandler<TEventArgs> handler
+        ) where TEventArgs : EventArgs
+        {
+            if (null == handler) return null;
+            return handler.Marshall<TEventArgs>(SynchronizationContext.Current);
+        }
+
+        /// <summary>
+        /// Marshalls an event handler to the specified synchronization context.
+        /// </summary>
+        /// <param name="handler">The handler to be marshalled.</param>
+        /// <param name="context">The synchronization context to which the handler should be marshalled.</param>
+        /// <returns>A new event handler proxy the marshalls the handler to the specified synchronization context.</returns>
+        public static EventHandler<TEventArgs> Marshall<TEventArgs>(
+            this EventHandler<TEventArgs> handler,
+            SynchronizationContext context
+        ) where TEventArgs : EventArgs
+        {
+            if (null == handler) return null;
+            return new EventHandler<TEventArgs>(
+                (sender, eventArgs) =>
+                {
+                    if (null == context)
+                    {
+                        handler(sender, eventArgs);
+                    }
+                    else
+                    {
+                        context.Post(
+                            new SendOrPostCallback(
+                                (o) =>
+                                {
+                                    var info = (EventHandlerMarshallingInfo<TEventArgs>)o;
+                                    handler(
+                                        info.Sender,
+                                        info.EventArgs
+                                    );
+                                }
+                            ),
+                            new EventHandlerMarshallingInfo<TEventArgs>()
+                            {
+                                Sender = sender,
+                                EventArgs = eventArgs
+                            }
+                        );
+                    }
+                }
+            );
+        }
+
+        /// <summary>
+        /// Event handler marshalling info used by the Marshall extension method.
+        /// </summary>
+        /// <typeparam name="TEventArgs"></typeparam>
+        private class EventHandlerMarshallingInfo<TEventArgs> where TEventArgs : EventArgs
+        {
+            public object Sender { get; set; }
+            public TEventArgs EventArgs { get; set; }
+        }
+
+        #endregion
+    }
+}
